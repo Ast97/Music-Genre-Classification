ea0ce4254a8393ce6e358e7d5c46dbb8b692c35c
Astha <asthabaranwalabc@gmail.com>
2019-03-25 19:10:22 +0530

    Update recommend.php

--- recommend.php
+++ recommend.php
@@ -2,7 +2,7 @@
 <html>
 <head> 
 <meta  http-equiv="Content-Type" content="text/html;  charset=iso-8859-1"> 
-<title>BLOOD BANK MANAGEMENT</title>
+<title>Song Recommendation</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="style.css">
